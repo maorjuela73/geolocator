9d54b27113a83b1397155acb93022950dafa0f03
Miguel Orjuela <ma.orjuela73@gmail.com>
2018-10-09 00:02:59 -0500

    Geolocator working

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +8,23 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  nlatitude: any;
+  nlongitude: any;
+
+  constructor(public navCtrl: NavController, private geolocation: Geolocation) {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.nlatitude = resp.coords.latitude
+      this.nlongitude = resp.coords.longitude
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+
+    let watch = this.geolocation.watchPosition();
+    watch.subscribe((data) => {
+     // data can be a set of coordinates, or an error (if an error occurred).
+     data.coords.latitude
+     data.coords.longitude
+    });
 
   }
 
